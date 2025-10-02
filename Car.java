da60a3b37fb8e83a59daa3d0216e1ef8dbdfb377
Diane Tuyizere <dtuyizere@smith.edu>
2025-10-02 19:47:00 -0400

    Set Engine attributes, accessors and methods.

--- Car.java
+++ Car.java
@@ -1,3 +1,20 @@
 public class Car {
+    int maxCapacity;
+    ArrayList<Passenger>passengersOnboard;
+    
+
+
+
+
+
+
+
+
+
+    /*int getCapacity();
+    int seatsRemaining();
+    Boolean addPassenger(Passenger p);
+    Boolean removePassenger(Passenger p); 
+    void printManifest() */
 
 }
--- Engine.java
+++ Engine.java
@@ -1,3 +1,40 @@
+
 public class Engine {
+
+    private String fuel_type;
+    private double capacity;
+    private double current_level;
+
+    /* The constructor */
+    public Engine(String fuel_type, double capacity, double current_level) {
+        this.fuel_type = fuel_type;
+        this.capacity = capacity;
+        this.current_level = current_level;
+    }
+
+    /* Accessors */
+    public fuelType getFuelType(String fuel_type) {
+        this.fuel_type = fuelType;
+        return fuelType;
+    }
+
+    public double getMaxfuel(double capacity) {
+        this.capacity = maxFuel;
+        return maxFuel;
+    }
+
+    public double getCurrentFuel(double current_level) {
+        this.current_level = currrentFuel;
+        return currentFuel;
+    }
+
+    /*methods */
+    public void refuel() {
+
+    }
+
+    public Boolean go() {
+
+    }
 
 }
