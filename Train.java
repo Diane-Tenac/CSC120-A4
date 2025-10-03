ab35fe0fcb37235fce5d7ed0680904dda7d9f65e
Diane Tuyizere <dtuyizere@smith.edu>
2025-10-02 20:17:12 -0400

    Added train atrributes,constructor,accessors and methods

--- Train.java
+++ Train.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+
 public class Train {
+    Engine engine;
+    ArrayList<Car> cars;
+/* Constructor */
+    public Train(FuelType fuelType, double fuelCapacity,int nCars, int passengerCapacity){
+
+    }
+/* Accessors :*/
+    public Engine getEngine(){
+}
+    public Car getCar(int i){
+}
+    public int getMaxCapacity(){
+}
+/* / Methods */
+    public int seatsRemaining() {
+}
+    public void printManifest(){
+
+}
+
 
 }
